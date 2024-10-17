544687ce4adbc48a0c72967d2ba3db6d63052c78
DatagenieSK <sk.alauddin100707@gmail.com>
2024-10-18 02:45:01 +0530

    fixed Errors in app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-#Importing Modules
+# Importing Modules
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
 import yfinance as yf
 from keras.models import load_model
 import streamlit as st
 
-
+# Define the date range
 start = '2010-01-01'
 end = '2024-10-18'
 
+# Streamlit app title
 st.title('Stock Trend Prediction')
+
+# User input for stock ticker
 user_input = st.text_input('Enter Stock Ticker', 'TSLA')
+
+# Fetching the stock data from Yahoo Finance
 df = yf.download(user_input, start=start, end=end)
 
-#describing data 
-
+# Displaying the stock data summary
 st.subheader('Data From 2010 to 18/10/2024')
 st.write(df.describe())
