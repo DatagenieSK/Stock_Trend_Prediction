8870d13a34a69714eca1fc9184ab649d49547474
DatagenieSK <sk.alauddin100707@gmail.com>
2024-10-18 03:04:44 +0530

    app,py updated

--- app.py
+++ app.py
@@ -57,3 +57,64 @@ plt.xlabel('Date')
 plt.ylabel('Price')
 plt.legend()
 st.pyplot(fig)
+
+
+#spliting the data into Training and Testing
+
+
+data_traning = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_traning_array = scaler.fit_transform(data_traning)
+
+x_train = []
+y_train = []
+
+for i in range(100,data_traning_array.shape[0]):
+    x_train.append(data_traning_array[i-100:i])
+    y_train.append(data_traning_array[i,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+
+#load Model
+model = load_model("Tensorflow_keras_model.h5")
+
+#testing part
+past_100_days = data_traning.tail(100)
+
+final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100:i]) 
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+
+y_predicted = model.predict(x_test)
+
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+
+#final Graph
+st.subheader('Predection Vs Original')
+plt.figure(figsize=(12,6))
+plt.plot(y_test,"b", label = "Original price")
+plt.plot(y_predicted,"r", label = "Predicted price")
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
