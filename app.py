dd2b8f388e31f20c1107d8bcdaed48c7f22f309d
DatagenieSK <sk.alauddin100707@gmail.com>
2024-10-18 03:06:33 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from keras.models import load_model
 import streamlit as st
-from sklearn.preprocessing import MinMaxScaler
 
 # Define the date range
 start = '2010-01-01'
@@ -59,53 +58,63 @@ plt.ylabel('Price')
 plt.legend()
 st.pyplot(fig)
 
-# Splitting the data into Training and Testing
-data_training = pd.DataFrame(df['Close'][0:int(len(df) * 0.70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df) * 0.70):int(len(df))])
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-data_training_array = scaler.fit_transform(data_training)
+#spliting the data into Training and Testing
+
+
+data_traning = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_traning_array = scaler.fit_transform(data_traning)
 
 x_train = []
 y_train = []
 
-for i in range(100, data_training_array.shape[0]):
-    x_train.append(data_training_array[i - 100:i])
-    y_train.append(data_training_array[i, 0])
+for i in range(100,data_traning_array.shape[0]):
+    x_train.append(data_traning_array[i-100:i])
+    y_train.append(data_traning_array[i,0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 
-# Load Model
+
+#load Model
 model = load_model("Tensorflow_keras_model.h5")
 
-# Testing part
-past_100_days = data_training.tail(100)
+#testing part
+past_100_days = data_traning.tail(100)
+
 final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
 
-input_data = scaler.transform(final_df)  # Use transform instead of fit_transform
+input_data = scaler.fit_transform(final_df)
 
 x_test = []
 y_test = []
 
+
 for i in range(100, input_data.shape[0]):
-    x_test.append(input_data[i - 100:i])
+    x_test.append(input_data[i-100:i]) 
     y_test.append(input_data[i, 0])
 
 x_test, y_test = np.array(x_test), np.array(y_test)
 
 y_predicted = model.predict(x_test)
 
-# Rescaling predicted and actual values back to original scale
-scale_factor = 1 / scaler.scale_[0]
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
 y_predicted = y_predicted * scale_factor
 y_test = y_test * scale_factor
 
-# Final Graph
-st.subheader('Prediction Vs Original')
-fig = plt.figure(figsize=(12, 6))
-plt.plot(y_test, label="Original price", color='blue')
-plt.plot(y_predicted, label="Predicted price", color='red')
+
+#final Graph
+st.subheader('Predection Vs Original')
+plt.figure(figsize=(12,6))
+plt.plot(y_test,"b", label = "Original price")
+plt.plot(y_predicted,"r", label = "Predicted price")
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
-st.pyplot(fig)  # Use st.pyplot instead of plt.show()
+plt.show()
